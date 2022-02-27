dbbd77f07802f6cb8cc2804f73ff1548a358685c
hajinopro <hajinopro@gmail.com>
2022-02-27 21:19:06 +0900

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by 하진호 on 2022/02/27.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
